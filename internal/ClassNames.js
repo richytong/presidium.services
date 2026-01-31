992d178c57f5432af00eaee840cdfc6126933feb
Richard <richytong@gmail.com>
2026-01-31 14:13:47 -0800

    pages

--- internal/ClassNames.js
+++ internal/ClassNames.js
@@ -2,7 +2,7 @@
 const ClassNames = function (lib) {
   const result = []
   for (const name in lib) {
-    if (typeof lib[name] == 'function') {
+    if (typeof lib[name] == 'function' || typeof lib[name] == 'object') {
       result.push(name)
       const fn = lib[name]
       for (const subName in fn) {
