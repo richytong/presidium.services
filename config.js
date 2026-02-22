168742dc46b748105c08413a7da06ae953ff03af
Richard <richytong@gmail.com>
2026-02-22 09:03:25 -0800

    migrate to cdn.jsdelivr.net

--- config.js
+++ config.js
@@ -10,13 +10,13 @@ module.exports = {
     ScriptTag('https://cdn.jsdelivr.net/npm/rubico@2.7.7/dist/rubico.min.js', {
       crossorigin: true,
     }),
-    ScriptTag('https://unpkg.com/react@18.3.0/umd/react.production.min.js', {
+    ScriptTag('https://cdn.jsdelivr.net/npm/react@18.3.0/umd/react.production.min.js', {
       crossorigin: true,
     }),
-    ScriptTag('https://unpkg.com/react-dom@18.3.0/umd/react-dom.production.min.js', {
+    ScriptTag('https://cdn.jsdelivr.net/npm/react-dom@18.3.0/umd/react-dom.production.min.js', {
       crossorigin: true,
     }),
-    ScriptTag('https://unpkg.com/arche@0.3.11/index.js', {
+    ScriptTag('https://cdn.jsdelivr.net/npm/arche@0.3.11/index.js', {
       crossorigin: true,
     }),
     ScriptTag('/global.js'),
